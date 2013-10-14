3f40d7ed8954abe44f8b8d89ac427a6a9c64581d
Leonardo Giroto <lgiroto@empresajunior.com.br>
2013-10-13 21:53:25 -0300

    Tentativa_1_SEQ_VIS

--- modulo_SEQ_VIS/Fontes/SEQ_VIS.c
+++ modulo_SEQ_VIS/Fontes/SEQ_VIS.c
@@ -15,5 +15,42 @@
 *
 *  $HA Histórico de evolução:
 *   Versão  Autor	Data			Observações
+*	1 		lg		13/out/2013		Criação e início do desenvolvimento do módulo
 *    
 ***************************************************************************/
+
+#include <LISTA.H>
+
+typedef LIS_tppLista SEQ_VIS_Coluna;
+typedef char* Carta;
+
+public SEQ_VIS_Coluna SEQ_VIS_CriaColuna(void)
+{
+	return LIS_CriarLista(NULL);
+}
+
+public int SEQ_VIS_ExcluirColuna(SEQ_VIS_Coluna coluna)
+{
+	LIS_DestruirLista(coluna);
+	return 0;
+}
+
+public int SEQ_VIS_InserirCarta (SEQ_VIS_Coluna destino, Carta carta)
+{
+	// checar se a carta na última posição da lista é 1 valor a menos e pertence a um naipe de cor oposta
+}
+
+public int SEQ_VIS_ExcluirCarta (SEQ_VIS_Coluna alvo, Carta carta)
+{
+	if(LIS_ProcurarValor(alvo, carta) == LIS_CondRetNaoEncontrou || LIS_ProcurarValor(alvo, carta) == LIS_CondRetListaVazia))
+	{
+		return -1;
+	}
+	else
+	{
+		int deuCerto = LIS_ExcluirElemento(alvo);
+	}
+
+	return deuCerto;
+}
+
--- modulo_SEQ_VIS/Fontes/SEQ_VIS.h
+++ modulo_SEQ_VIS/Fontes/SEQ_VIS.h
@@ -17,6 +17,7 @@
 *  $HA Histórico de evolução:
 *	Versão  Autor   Data			Observações
 *	1		nk		11/out/2013		Criação do módulo com suas definições
+*	2  		lg		13/out/2013		Realização das definições
 *
 *  $ED Descrição do módulo
 *	Implementa as regras e funções básicas às sequências visíveis de cartas.
@@ -28,3 +29,8 @@
 #else
    #define SEQ_VIS_EXT extern
 #endif
+
+public SEQ_VIS_Coluna SEQ_VIS_CriaColuna (void);
+public int SEQ_VIS_ExcluirColuna(SEQ_VIS_Coluna coluna );
+public int SEQ_VIS_InserirCarta (SEQ_VIS_Coluna destino, Carta carta);
+public int SEQ_VIS_ExcluirCarta (SEQ_VIS_Coluna alvo, Carta carta);
